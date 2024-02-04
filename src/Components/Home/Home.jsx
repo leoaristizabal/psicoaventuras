9887ed1734abd41b01e8e780431c4c39c64bde31
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2024-02-04 01:53:55 -0400

    first home component structure and animated brackground

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Home.css";
+import Social from "./Social";
+import Data from "./Data";
+
+const Home = () => {
+  return (
+    <section className="home section" id="home">
+      <div className="home__container container grid">
+        <div className="home__content grid">
+          <div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+            <div class="wave"></div>
+          </div>
+
+          <Social />
+
+          <div className="home__img"></div>
+
+          <Data />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
---
+++ src/Components/Home/Social.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="www.instagram.com/psicoaventuras"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+      <a href="" className="home__social-icon" target="_blank">
+        <i class="uil uil-envelope-add"></i>
+      </a>
+      <a href="www.linkedin.com" className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
