d728e6d7d5f844da0fb037be46ff260adda4560b
leoaristizabal <leonardo.aristizabal.m@gmail.com>
2024-02-04 06:35:53 -0400

    home component finish structure

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,24 +2,26 @@ import React from "react";
 import "./Home.css";
 import Social from "./Social";
 import Data from "./Data";
+import ScrollDown from "./ScrollDown";
 
 const Home = () => {
   return (
     <section className="home section" id="home">
+      <div id= "bg-animation"> {/*Waves de la animacion del barckground */}
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+      </div>
       <div className="home__container container grid">
         <div className="home__content grid">
-          <div>
-            <div class="wave"></div>
-            <div class="wave"></div>
-            <div class="wave"></div>
-          </div>
-
           <Social />
 
           <div className="home__img"></div>
 
           <Data />
         </div>
+
+        <ScrollDown/>
       </div>
     </section>
   );
